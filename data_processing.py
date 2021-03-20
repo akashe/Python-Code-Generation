dcc49f49bd747dba54890d1e97375393ff87fdf9
akashe <infiniteakashe@gmail.com>
2021-03-20 17:19:07 +0530

    fixed tokenizer for f'

--- data_processing.py
+++ data_processing.py
@@ -213,7 +213,12 @@ def getTokenizer(python_code):
         a = list(tokenize(BytesIO(python_code.encode('utf-8')).readline))
         for i__ in a[1:-1]:
             if i__.exact_type == 3:
-                string_tokens = [k__ for k__ in i__[1]]
+                if re.match(r'^f"', i__[1]):
+                    string_tokens = ['f"'] + [k__ for k__ in i__[1][2:]]
+                elif re.match(r"^f'", i__[1]):
+                    string_tokens = ["f'"] + [k__ for k__ in i__[1][2:]]
+                else:
+                    string_tokens = [k__ for k__ in i__[1]]
                 tokens = tokens + string_tokens
             elif i__.exact_type == 6:   # removing dedent tokens
                 continue
